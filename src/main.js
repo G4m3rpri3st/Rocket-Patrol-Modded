4696a339dfca47a993138bec28c5b985f774eaa2
G4m3rpri3st <61484305+G4m3rpri3st@users.noreply.github.com>
2022-04-19 23:50:17 -0700

    Added new artwork

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@
 * Track a high score that persists across scenes and display it in the UI (5)
 * Add your own (copyright-free) background music to the Play scene (5)
 * Display the time remaining (in seconds) on the screen (10)
+* Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (20) 
 * Implement a new timing/scoring mechanism that adds time to the clock for successful hits (20)
 * Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (20)
 */
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -25,13 +25,13 @@ class Play extends Phaser.Scene {
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
 
         // green UI background
-        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0).setDepth(1);
 
         // white borders
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0).setDepth(1);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0).setDepth(1);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0).setDepth(1);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0).setDepth(1);
 
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
@@ -70,12 +70,12 @@ class Play extends Phaser.Scene {
           },
           fixedWidth: 160
         }
-        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2.4, 'Score:' + this.p1Score, scoreConfig);
-        this.highScore = this.add.text(game.config.width/2, borderUISize + borderPadding*2.4, 'High:' + highScore, scoreConfig).setOrigin(0.5,0);
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2.4, 'Score:' + this.p1Score, scoreConfig).setDepth(2);
+        this.highScore = this.add.text(game.config.width/2, borderUISize + borderPadding*2.4, 'High:' + highScore, scoreConfig).setOrigin(0.5,0).setDepth(2);
 
         // display timer
         this.timeLeft = game.settings.gameTimer;
-        this.displayTimer = this.add.text(game.config.width - borderUISize - borderPadding, borderUISize + borderPadding*2.4, 'Time:' + this.timeLeft/1000, scoreConfig).setOrigin(1,0);
+        this.displayTimer = this.add.text(game.config.width - borderUISize - borderPadding, borderUISize + borderPadding*2.4, 'Time:' + this.timeLeft/1000, scoreConfig).setOrigin(1,0).setDepth(2);
 
         // GAME OVER flag
         this.gameOver = false;
